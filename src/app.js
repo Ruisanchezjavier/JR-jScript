3177befc3ae1606d157654531123ccfc5a95394d
Ruisanchezjavier <160360911+Ruisanchezjavier@users.noreply.github.com>
2024-04-08 23:23:08 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // 1. create the pats of the sentence arrays
+  let who = ["The dog", "The mailman", "The bird"];
+  let action = ["ate", "threw away", "mailed", "stole"];
+  let what = ["my homework", "my cellphone", "the car"];
+  let when = ["yesterday!", "this morning", "last week"];
+
+  function selectArrayElement(arrayName, arrayLength) {
+    let randomNumber = Math.floor(Math.random() * arrayLength);
+    let selectedElement = arrayName[randomNumber];
+    return selectedElement;
+  }
+
+  let randomWho = selectArrayElement(who, who.length);
+  let randomAction = selectArrayElement(action, action.length);
+  let randomWhat = selectArrayElement(what, what.length);
+  let randomWhen = selectArrayElement(when, when.length);
+
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  // 3. print the excuse on the terminal, and then in the browser
+  const htmlElement = document.querySelector(".excuse");
+  htmlElement.innerHTML = excuse;
 };
